f4454617f0127ae52f0c7194dde8ab08c5f40f7f
Barry Huang <barryhuang0813@gamil.com>
2022-07-06 10:46:02 -0700

    finished game

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate");
+    if (Character.classList != "animate"){
+        character.classList.add("animate");
+    }
     setTimeout(function(){
-        character.classList.remove(animate)
-    })
+        character.classList.remove("animate");
+    },500)
 }
+
+var checkDead = interval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft =
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft >0 && characterTop>=130){
+        block.style.animation = "none";
+        block.style.style.display = "none";
+        alert("loser");
+    }
+},10)
